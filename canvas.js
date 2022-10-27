5361f597b2735738bc5689f274dd5c6752f81372
peimanhp <peiman_hp@yahoo.com>
2022-10-27 10:04:05 +0330

    completed

--- canvas.js
+++ canvas.js
@@ -45,17 +45,39 @@ const c = canvas.getContext("2d");
 // }
 // animate();
 
+let mouse = {
+  x: undefined,
+  y: undefined,
+};
+let maxRadius = 50;
+let minRadius = 4;
+
+let colorArray = ["#55C2B6", "#3A6AA6", "#F2C16D", "#EC5A5A", "#663232"];
+
+window.addEventListener("mousemove", (e) => {
+  mouse.x = e.x;
+  mouse.y = e.y;
+});
+
+window.addEventListener("resize", () => {
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+});
+
 function Circle(x, y, radius, dx, dy) {
   this.x = x;
   this.y = y;
   this.dx = dx;
   this.dy = dy;
   this.radius = radius;
+  this.minRadius = radius;
+  this.color = colorArray[Math.floor(Math.random() * colorArray.length)];
 
   this.draw = function () {
     c.beginPath();
     c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, true);
-    c.stroke();
+    c.fillStyle = this.color;
+    c.fill();
   };
 
   this.update = function () {
@@ -70,31 +92,38 @@ function Circle(x, y, radius, dx, dy) {
     this.x += this.dx;
     this.y += this.dy;
 
+    if (
+      mouse.x - this.x < 50 &&
+      mouse.x - this.x > -50 &&
+      mouse.y - this.y < 50 &&
+      mouse.y - this.y > -50
+    ) {
+      if (this.radius < maxRadius) this.radius += 1;
+    } else if (this.radius > this.minRadius) this.radius -= 1;
+
     this.draw();
   };
 }
 
 let circleArray = [];
 
-for (let i = 0; i < 100; i++) {
-    let radius = 50;
-    let x = Math.random() * (innerWidth - radius * 2) + radius;
-    let y = Math.random() * (innerHeight - radius * 2) + radius;
-    let dx = Math.random() - 0.5 * 10;
-    let dy = Math.random() - 0.5 * 10;
+for (let i = 0; i < 1000; i++) {
+  let radius = Math.random() * minRadius + 1;
+  let x = Math.random() * (innerWidth - radius * 2) + radius;
+  let y = Math.random() * (innerHeight - radius * 2) + radius;
+  let dx = Math.random() - 0.5;
+  let dy = Math.random() - 0.5;
 
-    let circle = new Circle(x, y, radius, dx, dy);
-    circleArray.push(circle);
+  let circle = new Circle(x, y, radius, dx, dy);
+  circleArray.push(circle);
 }
-
-console.log(circleArray);
 
 function animate() {
   requestAnimationFrame(animate);
   c.clearRect(0, 0, innerWidth, innerHeight); //clear whole screen
 
   for (let i = 0; i < circleArray.length; i++) {
-      circleArray[i].update();    
+    circleArray[i].update();
   }
 }
 
